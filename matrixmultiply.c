a901ac3c60e006cbf1165c398894019aa33a1375
Kevin Wang <wankevin@login2.crusher.olcf.ornl.gov>
2022-08-12 10:30:48 -0400

    Updated to final version of matrixmultiply.c

--- matrixmultiply.c
+++ matrixmultiply.c
@@ -3,15 +3,16 @@
 #include <omp.h>
 #include <time.h>
 
-int normalMatrixMultiply(int matARow, int matACol, int matBRow, int matBCol, int matrixA[matARow][matACol], int matrixB[matBRow][matBCol]);
-int ompMatrixMultiply(int matARow, int matACol, int matBRow, int matBCol, int matrixA[matARow][matACol], int matrixB[matBRow][matBCol]);
-int ompOffloadingMatrixMultiply(int matARow, int matACol, int matBRow, int matBCol, int matrixA[matARow][matACol], int matrixB[matBRow][matBCol]);
+int normalMatrixMultiply(int matARow, int matACol, int matBRow, int matBCol, double matrixA[matARow][matACol], double matrixB[matBRow][matBCol]);
+int ompMatrixMultiply(int matARow, int matACol, int matBRow, int matBCol, double matrixA[matARow][matACol], double matrixB[matBRow][matBCol]);
+int ompOffloadingMatrixMultiply(int matARow, int matACol, int matBRow, int matBCol, double matrixA[matARow][matACol], double matrixB[matBRow][matBCol]);
 
 
 int main(int argc, char** argv) {
     double start;
     double end;
     int matARow, matACol, matBRow, matBCol;
+    int i, j, k;
 
     srand(time(0));
     
@@ -30,14 +31,37 @@ int main(int argc, char** argv) {
     printf("Please enter the number of columns of matrix B\n");
     scanf("%d", &matBCol);
 
-    int (*matrixA)[matACol] = malloc(sizeof(int[matARow][matACol]));
-    int (*matrixB)[matBCol] = malloc(sizeof(int[matBRow][matBCol]));
+    double (*matrixA)[matACol] = malloc(sizeof(double[matARow][matACol]));
+    double (*matrixB)[matBCol] = malloc(sizeof(double[matBRow][matBCol]));
 
     if (matACol != matBRow) {
         printf("Dimensions incompatible for matrix multiplication\n");
         return 1;
     }
 
+//Parallel way to enter random values for all values of matrices A and B
+#pragma omp parallel default(none) shared(matARow, matACol, matBRow, matBCol, matrixA, matrixB) private(i,j,k)
+    {
+	#pragma omp for schedule(guided) collapse(2)
+            for (i = 0; i < matARow; i++) {
+                for (j = 0; j < matACol; j++) {
+                    //printf("Please enter the element at row %d, column %d of matrix A\n", i, j);
+                    //scanf("%d", &(matrixA[i][j]));
+                    matrixA[i][j] = rand(); //CHANGE ARRAY TO DOUBLE
+                }
+            }
+    
+        #pragma omp for schedule(guided) collapse(2)
+            for (i = 0; i < matBRow; i++) {
+                for (j = 0; j < matBCol; j++) {
+                    //printf("Please enter the element at row %d, column %d of matrix B\n", i, j);
+                    //scanf("%d", &(matrixB[i][j]));
+                    matrixB[i][j] = rand();
+                }
+            }
+    }
+
+    //Recording the time before and after each function to record the time each takes
     start = omp_get_wtime();    
     normalMatrixMultiply(matARow, matACol, matBRow, matBCol, matrixA, matrixB);
     end = omp_get_wtime();
@@ -59,28 +83,13 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-int normalMatrixMultiply(int matARow, int matACol, int matBRow, int matBCol, int matrixA[matARow][matACol], int matrixB[matBRow][matBCol]) {
+//Function to perform matrix multiplication without any form of attempted speed-ups
+int normalMatrixMultiply(int matARow, int matACol, int matBRow, int matBCol, double matrixA[matARow][matACol], double matrixB[matBRow][matBCol]) {
     int i;
     int j;
     int k;
 
-    for (i = 0; i < matARow; i++) {
-        for (j = 0; j < matACol; j++) {
-            //printf("Please enter the element at row %d, column %d of matrix A\n", i, j);
-            //scanf("%d", &(matrixA[i][j]));
-            matrixA[i][j] = rand();
-        }
-    }
-
-    for (i = 0; i < matBRow; i++) {
-        for (j = 0; j < matBCol; j++) {
-            //printf("Please enter the element at row %d, column %d of matrix B\n", i, j);
-            //scanf("%d", &(matrixB[i][j]));
-            matrixB[i][j] = rand();
-        }
-    }
-
-    int (*matrixC)[matBCol] = malloc(sizeof(int[matARow][matBCol]));
+    double (*matrixC)[matBCol] = malloc(sizeof(double[matARow][matBCol]));
     for (i = 0; i < matARow; i++) {
         for (j = 0; j < matBCol; j++) {
             for (k = 0; k < matACol; k++) {
@@ -105,33 +114,14 @@ int normalMatrixMultiply(int matARow, int matACol, int matBRow, int matBCol, int
     return 0; 
 }
 
-int ompMatrixMultiply(int matARow, int matACol, int matBRow, int matBCol, int matrixA[matARow][matACol], int matrixB[matBRow][matBCol]) {
+//Function to perform matrix multiplicatiom under OMP parallel threading
+int ompMatrixMultiply(int matARow, int matACol, int matBRow, int matBCol, double matrixA[matARow][matACol], double matrixB[matBRow][matBCol]) {
     int i;
     int j;
     int k;
 
-#pragma omp parallel default(none) shared(matARow, matACol, matBRow, matBCol, matrixA, matrixB) private(i,j,k)
-    {
-	#pragma omp for schedule(guided) collapse(2)
-            for (i = 0; i < matARow; i++) {
-                for (j = 0; j < matACol; j++) {
-                    //printf("Please enter the element at row %d, column %d of matrix A\n", i, j);
-                    //scanf("%d", &(matrixA[i][j]));
-                    matrixA[i][j] = rand();
-                }
-            }
-    
-        #pragma omp for schedule(guided) collapse(2)
-            for (i = 0; i < matBRow; i++) {
-                for (j = 0; j < matBCol; j++) {
-                    //printf("Please enter the element at row %d, column %d of matrix B\n", i, j);
-                    //scanf("%d", &(matrixB[i][j]));
-                    matrixB[i][j] = rand();
-                }
-            }
-    }
 
-    int (*matrixC)[matBCol] = malloc(sizeof(int[matARow][matBCol]));
+    double (*matrixC)[matBCol] = malloc(sizeof(double[matARow][matBCol]));
 
 #pragma omp parallel default(none) shared(matARow, matBCol, matACol, matrixA, matrixB, matrixC) private(i,j,k)
 {
@@ -160,7 +150,8 @@ int ompMatrixMultiply(int matARow, int matACol, int matBRow, int matBCol, int ma
     return 0; 
 }
 
-int ompOffloadingMatrixMultiply(int matARow, int matACol, int matBRow, int matBCol, int matrixA[matARow][matACol], int matrixB[matBRow][matBCol]) {
+//Function to perform matrix multiplication with OMP GPU Offloading
+int ompOffloadingMatrixMultiply(int matARow, int matACol, int matBRow, int matBCol, double matrixA[matARow][matACol], double matrixB[matBRow][matBCol]) {
     int i;
     int j;
     int k;
@@ -169,31 +160,8 @@ int ompOffloadingMatrixMultiply(int matARow, int matACol, int matBRow, int matBC
 //include time for just the calculation and the time for the whole thing
 #pragma omp target enter data map (to: matrixA[0:matARow][0:matACol], matrixB[0:matBRow][0:matBCol])
 start = omp_get_wtime();
-#pragma omp target map(tofrom: matrixA)
-{
-    #pragma omp teams distribute parallel for simd num_teams(16) collapse(2)
-    for (i = 0; i < matARow; i++) {
-        for (j = 0; j < matACol; j++) {
-            //printf("Please enter the element at row %d, column %d of matrix A\n", i, j);
-            //scanf("%d", &(matrixA[i][j]));
-            matrixA[i][j] = rand();
-        }
-    }
-}
 
-#pragma omp target map(tofrom: matrixB)
-{
-    #pragma omp teams distribute parallel for simd num_teams(16) collapse(2)
-            for (i = 0; i < matBRow; i++) {
-                for (j = 0; j < matBCol; j++) {
-                    //printf("Please enter the element at row %d, column %d of matrix B\n", i, j);
-                    //scanf("%d", &(matrixB[i][j]));
-                    matrixB[i][j] = rand();
-                }
-            }
-}
-
-    int (*matrixC)[matBCol] = malloc(sizeof(int[matARow][matBCol]));
+double (*matrixC)[matBCol] = malloc(sizeof(double[matARow][matBCol]));
 
 #pragma omp target map(to: matrixA, matrixB) map(tofrom: matrixC)
 {
@@ -222,7 +190,3 @@ start = omp_get_wtime();
     return 0; 
 
 }
-
-//int hipMatrixMultiply(int matARow, int matACol, int matBRow, int matBCol, int matrixA[matARow][matACol], int matrixB[matBRow][matBCol]) {
-//
-//}
